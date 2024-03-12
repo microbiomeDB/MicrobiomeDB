c4db554f47579ef1ed024f7d701db622f13f6fb5
Danielle Callan <dcallan@upenn.edu>
2024-03-12 11:13:28 -0400

    add computes package reexports

---
+++ R/computes.R
@@ -0,0 +1,19 @@
+#' @importFrom microbiomeComputations rankedAbundance
+#' @export
+microbiomeComputations::rankedAbundance
+
+#' @importFrom microbiomeComputations alphaDiv
+#' @export
+microbiomeComputations::alphaDiv
+
+#' @importFrom microbiomeComputations betaDiv
+#' @export
+microbiomeComputations::betaDiv
+
+#' @importFrom microbiomeComputations differentialAbundance
+#' @export
+microbiomeComputations::differentialAbundance
+
+#' @importFrom microbiomeComputations correlation
+#' @export
+microbiomeComputations::correlation
